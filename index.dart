845aeecc8ac93059d2193958f20d2f15fa489348
kumanboy <husandavronov13@gmail.com>
2025-03-10 21:38:43 +0500

    variables

---
+++ index.dart
@@ -0,0 +1,13 @@
+void main() {
+  var name = "Husan"; // type inferred as String
+  int age = 21;// explicit type
+  double height = 1.85; // floating point
+  bool isStudent = true;
+
+
+  print('''
+           Hello Dart my name is $name 
+           and i am $age years old my height is $height
+           and i am student it is $isStudent
+         ''');
+}
